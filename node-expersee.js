ca92cf3a20e201f060b9abc4332e8097a2fa95a1
kerubel98 <kirubeltadesse98@gmail.com>
2024-04-12 23:02:23 +0300

    valdation

--- node-expersee.js
+++ node-expersee.js
@@ -1,12 +1,12 @@
 const express = require("express");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const courses = [
-    {id:1, name:"course1"},
-    {id:2, name:"course2"},
-    {id:3, name:"course3"},
-    {id:4, name:"course4"},
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
 ];
 
 app.get("/", (req, res) => {
@@ -16,22 +16,24 @@ app.get("/api/coures", (req, res) => {
   res.send(courses);
 });
 app.get("/api/coures/:id", (req, res) => {
-    const resualt = courses.find(c=>c.id === parseInt(req.params.id))
-    if(!resualt) res.status(404).send('The coures with the given ID is was not found')
-    res.send(resualt)
-  });
+  const resualt = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!resualt)
+    res.status(404).send("The coures with the given ID is was not found");
+  res.send(resualt);
+});
 
-app.post('/api/coureses/', (req, res) =>{
-    const corse = {
-        id:courses.length + 1,
-        name: req.body.name
-    };
-    courses.push(corse);
-    res.send(corse)
-})
-
+app.post("/api/coureses/", (req, res) => {
+  const corse = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  if (!req.body.name || req.name.length < 3) {
+    res.status(404).send("band request");
+    return;
+  }
+  courses.push(corse);
+  res.send(corse);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listing on port ${port} ..`));
-
- 
